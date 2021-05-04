b427a8f013cc53d8ea05b2cddcfce54b48dca223
Saloni Shah <saloshah01@gmail.com>
2021-05-04 11:04:24 +0530

    Add files via upload

---
+++ build/precache-manifest.04c7dc7416bc7d270936f730f10f5c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0636d60676ed463d03ab1a6dce2df7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a11190443918d5db6b4",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "9f16669a78388b1c7e2d",
+    "url": "/static/js/2.479629ae.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.479629ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a11190443918d5db6b4",
+    "url": "/static/js/main.aa8fc6f9.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
